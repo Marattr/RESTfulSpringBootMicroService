dfc95246d8af65376e606e8c2d6943556af4fed7
Murat Demirors <murat.demirors@hotmail.com>
2019-12-12 16:18:56 +0300

    Initial commit

---
+++ src/main/java/com/example/ec/repo/TourRatingRepository.java
@@ -0,0 +1,22 @@
+package com.example.ec.repo;
+
+import com.example.ec.domain.TourRating;
+import com.example.ec.domain.TourRatingPk;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+
+@RepositoryRestResource(exported = false)
+public interface TourRatingRepository extends CrudRepository<TourRating, TourRatingPk> {
+
+    List<TourRating> findByPkTourId(Integer tourId);
+
+    Page<TourRating> findByPkTourId(Integer tourId, Pageable pageable);
+
+    TourRating findByPkTourIdAndPkCustomerId(Integer tourId, Integer customerId);
+}
